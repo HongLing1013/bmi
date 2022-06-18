f4bbff65fe9f8a8a68696138672e9d1d4da7f8da
Hollie <tsubasababy1013@gmail.com>
2022-06-18 11:41:16 +0800

    BMI

---
+++ Computing.php
@@ -0,0 +1,36 @@
+<!-- 負責運算的網頁 -->
+<?php
+
+// 兩種接收方式
+if(empty($_GET)){ //判斷$_GET是否為空
+  $height=$_POST['height'];
+  $weight=$_POST['weight'];
+}else{
+  $height=$_GET['height'];
+  $weight=$_GET['weight'];
+};
+
+// 計算
+$bmi=round($weight/(($height/100)*($height/100)),1); // 取小數點後一位
+
+// 判斷
+$result='';
+
+if($bmi>=35){
+  $result= "重度肥胖";
+}else if($bmi >= 30 && $bmi < 35){
+  $result= "中度肥胖";
+}else if ($bmi >= 27 && $bmi < 30){
+  $result= "輕度肥胖";
+}else if($bmi >=24 && $bmi < 27 ){
+  $result= "過重";
+}else if ($bmi >=18.5 && $bmi < 24){
+  $result= "健康";
+}else if($bmi < 18.5 ){
+  $result= "過輕";
+}
+
+// 導向
+header("location:Result.php?bmi=$bmi&result=$result");
+
+?>
---
+++ Result.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>結果頁</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+
+    body {
+      background: url(./img/5d2fddbc63b3ed844fd9a3e308717a32_39193fcabd20cba835f71c720e97cbd2.jpg);
+      min-height: 100vh;
+      text-align: center;
+      justify-content: center;
+    }
+
+    h1 {
+      text-shadow: 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black;
+      color: #fff;
+      line-height: 20vh;
+    }
+
+    h3 {
+      text-shadow: 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black;
+  color:#fff;
+      line-height: 10vh;
+      margin-bottom: 10vh;
+    }
+
+    div {
+      text-shadow: 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black;
+      color: #fff;
+    }
+
+    button {
+      margin: 2vh;
+      border: 2px solid #000;
+      border-radius: 50px;
+      width: 25vh;
+      height: 5vh;
+      text-shadow: 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black;
+      color: #fff;
+      background: transparent;
+    }
+
+  </style>
+</head>
+
+<body>
+  <h1>你的BMI為<?= $_GET['bmi']; ?></h1>
+  <h3>判定結果為<?= $_GET['result']; ?></h3>
+  <div>
+    <a href="index.html"><button>回到BMI計算首頁</button></a>
+  </div>
+</body>
+
+</html>
