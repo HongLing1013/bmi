196036fd4b74b946adefd0531bbb4e5bc4fb4aad
Hollie <tsubasababy1013@gmail.com>
2022-06-18 12:00:08 +0800

    加了一層霧面效果

--- Result.php
+++ Result.php
@@ -14,9 +14,19 @@
 
     body {
       background: url(./img/5d2fddbc63b3ed844fd9a3e308717a32_39193fcabd20cba835f71c720e97cbd2.jpg);
+      background-attachment: fixed;
+      background-repeat: no-repeat;
+      background-position: top center;
+      background-size: 100%;
       min-height: 100vh;
       text-align: center;
       justify-content: center;
+    }
+
+    .bg {
+      background: hsla(0, 0%, 100%, .3);
+      height: 100vh;
+
     }
 
     h1 {
@@ -27,7 +37,7 @@
 
     h3 {
       text-shadow: 1px 1px black, -1px -1px black, 1px -1px black, -1px 1px black;
-  color:#fff;
+      color: #fff;
       line-height: 10vh;
       margin-bottom: 10vh;
     }
@@ -47,15 +57,17 @@
       color: #fff;
       background: transparent;
     }
-
   </style>
 </head>
 
 <body>
-  <h1>你的BMI為<?= $_GET['bmi']; ?></h1>
-  <h3>判定結果為<?= $_GET['result']; ?></h3>
-  <div>
-    <a href="index.html"><button>回到BMI計算首頁</button></a>
+  <div class="bg">
+
+    <h1>你的BMI為<?= $_GET['bmi']; ?></h1>
+    <h3>判定結果為<?= $_GET['result']; ?></h3>
+    <div>
+      <a href="index.html"><button>回到BMI計算首頁</button></a>
+    </div>
   </div>
 </body>
 
